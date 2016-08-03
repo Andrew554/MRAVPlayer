cb15adde1d11da1fb023171fbd87205d1c34d34e
Mr_洛先森 <897323459@qq.com>
2016-08-03 11:46:19 +0800

    MRVideoPlayer
    通过AVPlayer实现全屏播放切换, 进度拖动播放等

---
+++ MRVideoPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MRVideoPlayer
+//
+//  Created by Andrew554 on 16/8/3.
+//  Copyright © 2016年 Andrew554. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
